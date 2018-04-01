a7ebf1807c5d5f7b72facb28e2d19161998b0e18
Tatim1 <abrabd98@gmail.com>
2018-04-01 17:37:07 +0300

    navbar

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,36 +1,49 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+
+// 	
+	// $("body").on("click", ".navbar-toggler", function(e) {
+	// 	e.preventDefault();
+	// 	$("body").toggleClass("nav-opened");
+	// });
+
+	// $("body").on("click", ".nav-link", function(e) {
+	// 	e.preventDefault();
+	// 	$("body").toggleClass("nav-opened");
+	// });
+	
+
+// $('body').on('click', '.scrollTo', function(e){
+// 					e.preventDefault();
+// 					var target = $(this).attr('href'),
+// 						offset = Math.floor($(target).offset().top);
+// 					$('body').toggleClass('nav-opened');
+// 					$('html').animate({
+// 						scrollTop: offset
+// 					}, 500, function()
+// 					{
+// 						if (offset <= 0) {
+// 							$('header').removeClass('fixed-top');
+// 						} else {
+// 							$('header').addClass('fixed-top');							
+// 						}
+// 					});
+// 				})
 
 	
-	$("body").on("click", ".toggle-nav", function(e) {
-		e.preventDefault();
-		$("body").toggleClass("nav-opened");
-	});
-
-	$("body").on("click", ".nav-link", function(e) {
-		e.preventDefault();
-		$("body").toggleClass("nav-opened");
-	});
-
-
-$('body').on('click', '.scrollTo', function(e){
-					e.preventDefault();
-					var target = $(this).attr('href'),
-						offset = Math.floor($(target).offset().top);
-					$('body').toggleClass('nav-opened');
-					$('html').animate({
-						scrollTop: offset
-					}, 500, function()
-					{
-						if (offset <= 0) {
-							$('header').removeClass('navbar');
-						} else {
-							$('header').addClass('navbar');							
-						}
-					});
-				})
-
 	
-	});
+// var map;
+// 		var myLatLng = {lat: -7.9307317, lng: 112.6376249};
+//       function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: myLatLng,
+//           zoom: 14
+//         });
+// 		  var marker = new google.maps.Marker({
+// 			  position: myLatLng,
+// 			  map: map,
+// 			  title: "creative",
+// 			  icon: "img/map-pin.png",
+// 		  });
+//       }
 
-
-	
+	// });
